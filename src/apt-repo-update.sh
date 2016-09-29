83322c6803d28ebbd57a69f94655cac135a4eb1f
Alexey Kamenskiy <alexey.kamenskiy@chinanetcloud.com>
2016-09-29 17:01:15 +0800

    Cleaning code / following best practices

--- src/apt-repo-update.sh
+++ src/apt-repo-update.sh
@@ -8,29 +8,29 @@ GPG_NAME=${3}
 VERSION=6.0
 
 for bindir in `find ${REPODIR}/dists/${REPONAME} -type d -name "binary*"`; do
-    arch=`echo $bindir|cut -d"-" -f 2`
+    arch=`echo ${bindir}|cut -d"-" -f 2`
     echo "Processing ${bindir} with arch ${arch}"
 
     overrides_file=/tmp/overrides
-    package_file=$bindir/Packages
-    release_file=$bindir/Release
+    package_file=${bindir}/Packages
+    release_file=${bindir}/Release
 
     # Create simple overrides file to stop warnings
-    cat /dev/null > $overrides_file
+    cat /dev/null > ${overrides_file}
     for pkg in `ls ${REPODIR}/pool/main/ | grep -E "(all|${arch})\.deb"`; do
         pkg_name=`/usr/bin/dpkg-deb -f pool/main/${pkg} Package`
-        echo "${pkg_name} Priority extra" >> $overrides_file
+        echo "${pkg_name} Priority extra" >> ${overrides_file}
     done
 
     # Index of packages is written to Packages which is also zipped
-    dpkg-scanpackages -a ${arch} ${REPODIR}/pool/main $overrides_file > $package_file
+    dpkg-scanpackages -a ${arch} ${REPODIR}/pool/main ${overrides_file} > ${package_file}
     # The line above is also commonly written as:
     # dpkg-scanpackages -a ${arch} pool/main /dev/null > $package_file
-    gzip -9c $package_file > ${package_file}.gz
-    bzip2 -c $package_file > ${package_file}.bz2
+    gzip -9c ${package_file} > ${package_file}.gz
+    bzip2 -c ${package_file} > ${package_file}.bz2
 
     # Cleanup
-    rm $overrides_file
+    rm ${overrides_file}
 done
 
 # Release info goes into Release & Release.gpg which includes an md5 & sha1 hash of Packages.*
@@ -61,5 +61,5 @@ for hashme in `find main -type f`; do
 done
 
 # Sign!
-gpg --yes -u $GPG_NAME --sign -bao Release.gpg Release
+gpg --yes -u ${GPG_NAME} --sign -bao Release.gpg Release
 cd -
