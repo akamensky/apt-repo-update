92bd25e8f6ea446db65a2cd0c1369a9281d1f759
Alexey Kamenskiy <alexey.kamenskiy@chinanetcloud.com>
2016-09-29 18:50:57 +0800

    Allow multiple releases to work in one repo (split by /pool/${REPONAME}/main)

--- src/apt-repo-update.sh
+++ src/apt-repo-update.sh
@@ -7,9 +7,41 @@ REPONAME=${2}
 GPG_NAME=${3}
 VERSION=6.0
 
-for bindir in `find ${REPODIR}/dists/${REPONAME} -type d -name "binary*"`; do
+# Check for ${REPODIR} and exit if does not exist!
+if [ ! -d ${REPODIR} ] ; then
+    echo "${REPODIR} is not a directory or does not exist"
+    exit 1
+fi
+
+cd ${REPODIR}
+
+# Check for dists/${REPONAME} and create
+if [ ! -d dists/${REPONAME} ] ; then
+    echo "Creating dists/${REPONAME} ..."
+    mkdir -p dists/${REPONAME}
+fi
+
+# Check for dists/${REPONAME}/main/binary-i386 and create
+if [ ! -d dists/${REPONAME}/main/binary-i386 ] ; then
+    echo "Creating dists/${REPONAME}/main/binary-i386 ..."
+    mkdir -p dists/${REPONAME}/main/binary-i386
+fi
+
+# Check for dists/${REPONAME}/main/binary-amd64 and create
+if [ ! -d dists/${REPONAME}/main/binary-amd64 ] ; then
+    echo "Creating dists/${REPONAME}/main/binary-amd64 ..."
+    mkdir -p dists/${REPONAME}/main/binary-amd64
+fi
+
+# Check for pool/${REPONAME}/main and create
+if [ ! -d pool/${REPONAME}/main ] ; then
+    echo "Creating pool/${REPONAME}/main ..."
+    mkdir -p pool/${REPONAME}/main
+fi
+
+for bindir in `find dists/${REPONAME} -type d -name "binary*"`; do
     arch=`echo ${bindir}|cut -d"-" -f 2`
-    echo "Processing ${bindir} with arch ${arch}"
+    echo "Processing arch ${arch}"
 
     overrides_file=/tmp/overrides
     package_file=${bindir}/Packages
@@ -17,15 +49,15 @@ for bindir in `find ${REPODIR}/dists/${REPONAME} -type d -name "binary*"`; do
 
     # Create simple overrides file to stop warnings
     cat /dev/null > ${overrides_file}
-    for pkg in `ls ${REPODIR}/pool/main/ | grep -E "(all|${arch})\.deb"`; do
-        pkg_name=`/usr/bin/dpkg-deb -f pool/main/${pkg} Package`
+    for pkg in `ls pool/${REPONAME}/main/ | grep -E "(all|${arch})\.deb"`; do
+        pkg_name=`/usr/bin/dpkg-deb -f pool/${REPONAME}/main/${pkg} Package`
         echo "${pkg_name} Priority extra" >> ${overrides_file}
     done
 
     # Index of packages is written to Packages which is also zipped
-    dpkg-scanpackages -a ${arch} ${REPODIR}/pool/main ${overrides_file} > ${package_file}
+    dpkg-scanpackages -a ${arch} pool/${REPONAME}/main ${overrides_file} > ${package_file}
     # The line above is also commonly written as:
-    # dpkg-scanpackages -a ${arch} pool/main /dev/null > $package_file
+    # dpkg-scanpackages -a ${arch} pool/${REPONAME}/main /dev/null > $package_file
     gzip -9c ${package_file} > ${package_file}.gz
     bzip2 -c ${package_file} > ${package_file}.bz2
 
@@ -35,8 +67,8 @@ done
 
 # Release info goes into Release & Release.gpg which includes an md5 & sha1 hash of Packages.*
 # Generate & sign release file
-cd ${REPODIR}/dists/${REPONAME}
-cat > Release <<ENDRELEASE
+# cd ${REPODIR}/dists/${REPONAME}
+cat > dists/${REPONAME}/Release <<ENDRELEASE
 Suite: ${REPONAME}
 Version: ${VERSION}
 Component: main
@@ -47,21 +79,21 @@ Date: `date`
 ENDRELEASE
 
 # Generate hashes
-echo "MD5Sum:" >> Release
-for hashme in `find main -type f`; do
+echo "MD5Sum:" >> dists/${REPONAME}/Release
+for hashme in `find dists/${REPONAME}/main -type f`; do
     md5=`openssl dgst -md5 ${hashme}|cut -d" " -f 2`
     size=`stat -c %s ${hashme}`
-    echo " ${md5} ${size} ${hashme}" >> Release
+    echo " ${md5} ${size} ${hashme}" >> dists/${REPONAME}/Release
 done
-echo "SHA1:" >> Release
-for hashme in `find main -type f`; do
+echo "SHA1:" >> dists/${REPONAME}/Release
+for hashme in `find dists/${REPONAME}/main -type f`; do
     sha1=`openssl dgst -sha1 ${hashme}|cut -d" " -f 2`
     size=`stat -c %s ${hashme}`
-    echo " ${sha1} ${size} ${hashme}" >> Release
+    echo " ${sha1} ${size} ${hashme}" >> dists/${REPONAME}/Release
 done
 
 # Sign!
 read -sp 'GPG Password: ' gpgpass
-gpg --batch --passphrase ${gpgpass} --yes -u ${GPG_NAME} --sign -bao Release.gpg Release
+gpg --batch --passphrase ${gpgpass} --yes -u ${GPG_NAME} --sign -bao dists/${REPONAME}/Release.gpg dists/${REPONAME}/Release
 echo "DONE"
 cd -
